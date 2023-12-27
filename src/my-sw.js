6646304a755fccc0ea74d03bafb44b4f46435d39
aditya-kumarr <127249885+aditya-kumarr@users.noreply.github.com>
2023-12-27 05:46:43 +0530

    fixed service worker that failed build

--- src/my-sw.js
+++ src/my-sw.js
@@ -1,5 +1,18 @@
-console.log("hello from service worker");
-console.log(self.registration);
+// sw.ts
+import { precacheAndRoute } from "workbox-precaching";
+import { registerRoute } from "workbox-routing";
+import { CacheFirst } from "workbox-strategies";
+
+precacheAndRoute(self.__WB_MANIFEST); // does compile
+registerRoute(
+  // Use a regular expression to match a specific URL pattern
+  /\/api\/data/,
+  // Use the CacheFirst strategy to attempt to fetch from the cache first, falling back to the network
+  new CacheFirst({
+    cacheName: "api-cache",
+    plugins: [],
+  })
+);
 
 self.addEventListener("push", (event) => {
   // Parse the incoming data from the push notification
